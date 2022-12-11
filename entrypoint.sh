05e0913d9d88aefb21447280b3bd3175ad3b0509
netcode <erik@heimdal.dev>
2022-12-12 00:39:45 +0100

    fix: :bug: fix the entrypoint script raising an error in CI

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,21 +18,8 @@ git config --global --add safe.directory /github/workspace
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
-# Unset the "errexit" option for the shell, which allows the script to continue even if any of the commands return a
-# non-zero exit code. This is used to prevent the script from exiting if the git diff-index command returns a non-zero
-# exit code, which is expected if there are changes to the requirements.txt file.
-set +e
-
 # Check if there are any changes to the requirements.txt file
-if git diff-index --quiet HEAD --; then
-  # If there are no changes, print a message and exit the script
-  echo "No changes to requirements.txt"
-  exit 0
-else
-  # If there are changes, enable the "errexit" option for the shell again, which causes the script to exit if any
-  # commands return a non-zero exit code. This is used to prevent the script from continuing if any errors occur
-  # during the commit and push operations.
-  set -e
+if [[ `git status --porcelain --untracked-files=no` ]]; then
   # Stage, commit, and push the changes to the requirements.txt file to the remote Git repository. The INPUT_COMMIT_MESSAGE
   # input parameter is used to specify the commit message for the changes.
   # Stage the changes to the requirements.txt file
@@ -44,4 +31,8 @@ else
 
   # Push the new commit to the remote Git repository, using the origin remote and the HEAD reference.
   git push -u origin HEAD
+else
+  # If there are no changes, print a message and exit the script
+  echo "No changes to requirements.txt"
+  exit 0
 fi
