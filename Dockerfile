446ca539aa02135b9795741c01fbdc7a98769ebe
netcode <erik@heimdal.dev>
2022-12-11 22:30:31 +0100

    feat: :arrow_up: use python 3.10

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,20 @@
-FROM alpine/git:1.0.7
+# Use the alpine:3.17 base image
+FROM alpine:3.17
 
+# Copy the LICENSE, README.md, and entrypoint.sh files from the local file system into the root directory of the Docker image
 COPY LICENSE README.md entrypoint.sh /
 
-RUN apk add python3
+# Install Python 3 on the Docker image
+RUN apk add --no-cache python3
+
+# Install Git on the Docker image
+RUN apk add git
+
+# Install the pipreqs Python package
 RUN pip3 install pipreqs
 
+# Make the entrypoint.sh script executable
 RUN chmod +x /entrypoint.sh
 
+# Set the default command to be run when a container is started from the Docker image
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,19 @@
-#!/bin/sh -l
+#!/usr/bin/env sh
 
+# Run the pipreqs command with the --force and --savepath options, using the INPUT_REQUIREMENT_PATH and INPUT_PROJECT_PATH
+# input parameters to specify the path to the requirements.txt file and the path to the project directory, respectively.
+# This generates the requirements.txt file for the project.
 pipreqs --force --savepath $INPUT_REQUIREMENT_PATH $INPUT_PROJECT_PATH
 
+# Set the -e option for the shell, which causes the script to exit if any commands return a non-zero exit code.
+# This is used to prevent the script from continuing if any errors occur during execution.
 set -e
-sh -c "ls"
 
-sh -c "git config --global user.name '${GITHUB_ACTOR}' \
-      && git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com'"
-echo `git add -A && git commit -m "Updated $INPUT_PROJECT_NAME requirements file" && git push -u origin HEAD`
+# Set the Git user name and email address to the GITHUB_ACTOR environment variable, which specifies the user or organization
+# that triggered the workflow. This is used to configure the Git credentials for the user who is running the workflow.
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+
+# Stage, commit, and push the changes to the requirements.txt file to the remote Git repository. The INPUT_COMMIT_MESSAGE
+# input parameter is used to specify the commit message for the changes.
+git add -A && git commit -m "$INPUT_COMMIT_MESSAGE" && git push -u origin HEAD
